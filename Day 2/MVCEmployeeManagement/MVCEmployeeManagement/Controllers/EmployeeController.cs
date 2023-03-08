db81c3f1ad51cc89dac5841b9b2f6b5526d4b8a4
Nikhil <nikhilshah.net@hotmail.com>
2023-03-08 16:33:34 +0530

    Day 2

---
+++ Day 2/MVCEmployeeManagement/MVCEmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCEmployeeManagement.Models;
+
+namespace MVCEmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        EmployeeModel eObj = new EmployeeModel(); //this is old way, not recommeneded, use Dependency Injection instead
+
+
+        public IActionResult EmployeeList()
+        {
+            ViewBag.employees = eObj.GetAllEmployees();
+            return View();
+        }
+
+        //this will give the page with text box and button
+        public IActionResult SearchEmployee()
+        {
+            ViewBag.isEmpSeach = false;
+            ViewBag.errMessage = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchEmployee(int employeeNo)
+        {
+            try
+            {
+                ViewBag.employee = eObj.SearchById(employeeNo);
+                ViewBag.isEmpSeach = true;
+                ViewBag.errMessage = "";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.isEmpSeach = false;
+                ViewBag.errMessage = ex.Message;                
+            }
+            return View();
+        }
+
+        [HttpGet] // this is anyways by default, so if u do not mention this, things will still work
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost] //this is mandatory
+        public IActionResult AddEmployee(EmployeeModel newEmp)
+        {
+            ViewBag.addResult = eObj.AddEmployee(newEmp);
+            // return View();
+          return  RedirectToAction("EmployeeList");
+        }
+
+
+        public IActionResult FilterByDesigantion()
+        {
+            ViewBag.filtered = false;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FilterByDesigantion(string edesignation)
+        {
+            var employees = eObj.FilterByDesignation(edesignation);
+            ViewBag.filtered = true;
+            return View();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Day 2/MVCEmployeeManagement/MVCEmployeeManagement/Models/EmployeeModel.cs
@@ -0,0 +1,78 @@
+﻿namespace MVCEmployeeManagement.Models
+{
+    public class EmployeeModel
+    {
+        public int empNo { get; set; }
+        public string empName { get; set; }
+        public string empDesignation { get; set; }
+        public double empSalary { get; set; }
+        public bool empIsPermenant { get; set; }
+
+        static List<EmployeeModel> eList = new List<EmployeeModel>()
+        {
+            new EmployeeModel(){ empNo=101, empName="Sahil", empDesignation="Sales", empIsPermenant=true, empSalary=6000 },
+            new EmployeeModel(){ empNo=102, empName="Karan", empDesignation="HR", empIsPermenant=true, empSalary=16000 },
+            new EmployeeModel(){ empNo=103, empName="Priya", empDesignation="Sales", empIsPermenant=false, empSalary=12000 },
+            new EmployeeModel(){ empNo=104, empName="Riya", empDesignation="HR", empIsPermenant=true, empSalary=19000 },
+            new EmployeeModel(){ empNo=105, empName="Anand", empDesignation="Sales", empIsPermenant=false, empSalary=22000 }
+        };
+
+        public List<EmployeeModel> GetAllEmployees()
+        {
+            return eList;
+        }
+
+        public EmployeeModel SearchById(int id)
+        {
+            var emp = eList.Find(e => e.empNo == id);
+
+            if (emp != null)
+            {
+                return emp;
+            }
+            throw new Exception("Employee Not Found");
+        }
+
+
+        public List<EmployeeModel> FilterByDesignation(string designation)
+        {
+            var emp = eList.FindAll(em => em.empDesignation == designation);
+            return emp;
+        }
+
+        public string AddEmployee(EmployeeModel newEmp)
+        {
+            eList.Add(newEmp);
+            return "Employee Added Successfully";
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
