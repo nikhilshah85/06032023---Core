fd13dab0648c52a7a17e934645bcc2a7e02a235c
Nikhil <nikhilshah.net@hotmail.com>
2023-03-10 16:25:20 +0530

    Day 4 - DI demo

---
+++ Day 4/DI_Demo/DI_Demo/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DI_Demo.Models;
+namespace DI_Demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        //this is not expected, use DI instead
+        //Employee empObj = new Employee();
+
+        Employee empObj;
+
+                                 //framework will inject the reference of object created 
+        public EmployeeController(Employee empObjREF)
+        {
+            empObj = empObjREF;
+        }
+
+
+        public IActionResult DisplayEmployee()
+        {
+            ViewBag.empList = empObj.GetAllEmployees(); //to execute the method u need object
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Day 4/DI_Demo/DI_Demo/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace DI_Demo.Models
+{
+    public class Employee
+    {
+        public int empNo { get; set; }
+        public string empName { get; set; }
+        public double empSalary { get; set; }
+        public string empDesignation { get; set; }
+        public bool empIsPermenant { get; set; }
+
+        static List<Employee> eList = new List<Employee>()
+        {
+            new Employee(){ empName ="Nikhil", empNo=101, empDesignation="Developer", empIsPermenant=true, empSalary=2000 },
+            new Employee(){ empName ="Manish", empNo=102, empDesignation="Developer", empIsPermenant=true, empSalary=2000 },
+            new Employee(){ empName ="Kirthi", empNo=103, empDesignation="Developer", empIsPermenant=true, empSalary=2000 },
+            new Employee(){ empName ="Raman", empNo=104, empDesignation="Developer", empIsPermenant=true, empSalary=2000 },
+            new Employee(){ empName ="Pari", empNo=105, empDesignation="Developer", empIsPermenant=true, empSalary=2000 },
+        };
+
+        public List<Employee> GetAllEmployees()
+        {
+            return eList;
+        }
+    }
+}
